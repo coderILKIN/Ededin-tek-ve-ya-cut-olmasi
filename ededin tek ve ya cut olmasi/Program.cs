828a968cdf4b9657784a53e5bf496d79fa7a1ed4
coderILKIN <tu758403c@code.edu.az>
2023-10-27 17:44:04 +0400

    first commit

---
+++ ededin tek ve ya cut olmasi/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace ededin_tek_ve_ya_cut_olmasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* 1ci yazdigim */
+
+            //Console.WriteLine("ekrana bir reqem yaz");
+
+            //int n;
+
+
+            //for (int i = 1; i>0; i++)
+            //{
+            //    n = Convert.ToInt32(Console.ReadLine());
+            //    if (n >= 0)
+            //    {
+
+            //        if (n == 0)
+            //        {
+            //            Console.WriteLine("zehmet olmasa 0 dan ferqli eded qeyd edin");
+            //        }
+
+            //        else if (n % 2 == 0)
+            //        {
+            //            Console.WriteLine("n ededi cutdur");
+            //        }
+
+            //        else
+            //        {
+            //            Console.WriteLine("n ededi tekdir");
+            //        }
+
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("musbet ede yaz");
+            //    }
+            //}
+
+            /* 2ci yazdigim */
+
+            //Console.WriteLine("--- ekrana bir eded yaz ----");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //if (n == 0)
+            //{
+            //    Console.WriteLine("0 dan ferqli eded yaz");
+            //}
+
+            //if (n < 0)
+            //{
+            //    Console.WriteLine("musbet eded yaz");
+            //}
+
+            //else if (n % 2 == 0)
+            //{
+            //    Console.WriteLine("n ededi cutdur");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("n ededi tekdir");
+            //}
+
+
+
+
+            /* 3cu yazdigim */
+
+            //Console.WriteLine("--- ekrana bir eded yaz ----");
+            //int n;
+
+            //for (int i = 1; i > 0; i++)
+            //{
+            //   n =  Convert.ToInt32(Console.ReadLine());
+            //    if (n == 0)
+            //    {
+            //        Console.WriteLine("0 dan ferqli eded yaz");
+            //    }
+
+            //    if (n < 0)
+            //    {
+            //        Console.WriteLine("musbet eded yaz");
+            //    }
+
+            //    else if (n % 2 == 0)
+            //    {
+            //        Console.WriteLine("n ededi cutdur");
+            //    }
+
+            //    else
+            //    {
+            //        Console.WriteLine("n ededi tekdir");
+            //    }
+            //}
+
+
+
+
+
+
+
+        }
+    }
+}
